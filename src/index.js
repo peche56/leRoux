77733e9ad05ea933221a51be1df69015f2f33c01
Clement Filleul <c.filleul@icloud.com>
2017-09-16 12:09:49 +0200

    [INIT] INstall redux devTools.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
+import {createStore} from 'redux';
+import {connectRouter, routerMiddleware} from 'connected-react-router';
+// import {provider} from 'react-redux';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(
+ window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+render(
+  <provider store={store}>
+    <App />
+  </provider>,
+  document.getElementById('root'));
